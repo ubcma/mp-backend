4dbe883fe673c5231c88a1e37ee94a5d25f22260
ETHAN <ethan.t.hansen@gmail.com>
2025-05-27 10:32:51 -0700

    fix: allowed cors origins

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,26 @@ import eventRouter from "./routes/eventRoutes";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.FRONTEND_URL,
-  credentials: true,
-}));
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "https://membership.ubcma.ca",
+  "https://membership-portal-git-ethan-mp-17-admin-65a1ff-ubcmas-projects.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:4000",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.all('/api/auth/*splat', toNodeHandler(auth));
 app.use(express.json());
